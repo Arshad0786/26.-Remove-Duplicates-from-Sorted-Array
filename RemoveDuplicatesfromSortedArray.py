5fa66bb3f48f72f023e1bbd54ce97172dc9a928e
Arshad0786 <js07150715@gmail.com>
2020-04-29 17:16:25 +0800

    improve logic and algorithm

--- RemoveDuplicatesfromSortedArray.py
+++ RemoveDuplicatesfromSortedArray.py
@@ -1,12 +1,8 @@
 class Solution:
     def removeDuplicates(self, nums):
         NewArrayIndex = 0
-        OldArrayIndex = 0
-        if(len(nums) < 2):
-            return len(nums)
-        for OldArrayIndex in range(len(nums)-1):
-            if(nums[OldArrayIndex] != nums[OldArrayIndex + 1]):
-                nums[NewArrayIndex] = nums[OldArrayIndex]
+        for OldArrayIndex in range(len(nums)):
+            if(nums[NewArrayIndex] != nums[OldArrayIndex]):
                 NewArrayIndex = NewArrayIndex + 1
-        nums[NewArrayIndex] = nums[OldArrayIndex + 1]
+                nums[NewArrayIndex] = nums[OldArrayIndex]
         return NewArrayIndex + 1
